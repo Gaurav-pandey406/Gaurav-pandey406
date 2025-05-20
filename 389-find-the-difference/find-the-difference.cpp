ee0490095af917ee53f60e5d268342a72cd342c0
Gaurav pandey <gauravpandey1741b@gmail.com>
2025-05-19 23:46:21 -0700

    Time: 3 ms (14.47%) | Memory: 9.3 MB (30.79%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> m;
+        
+    
+        for(char c : s) {
+            m[c]++;
+        }
+        
+        
+        for(char c : t) {
+            if(m.find(c) == m.end() || m[c] == 0) {
+                return c;  
+            }
+            m[c]--; 
+        }
+        
+        return ' ';
+    }
+};
