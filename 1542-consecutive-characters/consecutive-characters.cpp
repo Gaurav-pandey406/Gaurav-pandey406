a4b7d2ad104e15abbe4ec11c7ed78b3f1e058362
Gaurav pandey <gauravpandey1741b@gmail.com>
2025-09-24 09:59:36 -0700

    Time: 0 ms (100.00%) | Memory: 9.6 MB (45.75%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int maxi = 1;
+        int count = 1;
+        for(int i = 1; i<s.length();i++){
+            if(s[i] == s[i-1]){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            maxi = max(count , maxi);
+
+        }
+        return maxi;
+        
+    }
+};
